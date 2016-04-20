783bb8d238dc86a60fc262e13225012d32ca68ea
kstukenborg <@stuk>
2016-04-20 08:46:02 -0700

    works except in landscape mode

--- MemeEditorView.swift
+++ MemeEditorView.swift
@@ -8,13 +8,18 @@
 
 import Foundation
 import UIKit
-
+import AVFoundation
+/*
+ The MemeEditorView class allows you to choose a picture, or take a picture and edit two text fields so that you
+ create a meme.  It also allows you to save it into a meme array.
+ */
 class MemeEditorView: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate{
     @IBOutlet weak var topTextField: UITextField!
     @IBOutlet weak var bottomTextField: UITextField!
     @IBOutlet weak var takePictureOutlet: UIBarButtonItem!
     @IBOutlet weak var pictureDisplayed: UIImageView!
     @IBOutlet weak var shareOutlet: UIBarButtonItem!
+    var didEdit :Bool = false
     
     let memeTextAttributes = [
         //black outline
@@ -27,212 +32,213 @@ class MemeEditorView: UIViewController, UIImagePickerControllerDelegate, UINavig
         NSStrokeWidthAttributeName : -3
         // NSStrokeWidthAttributeName : 0
     ]
-    struct Meme {
-        
-        var topText : String
-        var bottomText : String
-        var originalImage : UIImage
-        var memedImage : UIImage
-        
-    }
-    
     override func viewDidLoad() {
-        print("viewDidLoad")
+
         super.viewDidLoad()
+        tabBarController?.tabBar.hidden = true
+
+         didEdit  = false
         let navigationController = UINavigationController()
         navigationController.delegate = self
-        self.topTextField.delegate = self
-        self.bottomTextField.delegate = self
+        
+        
+        topTextField.delegate = self
+        bottomTextField.delegate = self
         shareOutlet.enabled = false
-        topTextField.defaultTextAttributes = memeTextAttributes
-        bottomTextField.defaultTextAttributes = memeTextAttributes
-        topTextField.textAlignment  = NSTextAlignment.Center
-        topTextField.backgroundColor = UIColor.clearColor()
-        topTextField.autocapitalizationType = UITextAutocapitalizationType.AllCharacters
-        bottomTextField.autocapitalizationType = UITextAutocapitalizationType.AllCharacters
+        setTextFieldAttributes(topTextField)
+        setTextFieldAttributes(bottomTextField)
         topTextField.text = "TOP"
-        bottomTextField.backgroundColor = UIColor.clearColor()
         bottomTextField.text  = "BOTTOM"
-        bottomTextField.textAlignment  = NSTextAlignment.Center
         takePictureOutlet.enabled = UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)
         
-        self.navigationController?.toolbarHidden = false
         self.navigationController?.view.backgroundColor = UIColor.blackColor()
         self.dismissViewControllerAnimated(true, completion: nil)
+        topTextField.enabled = false
+        bottomTextField.enabled = false
+        
+    }
+    func setTextFieldAttributes (textField: UITextField){
+        textField.defaultTextAttributes = memeTextAttributes
+        textField.textAlignment = NSTextAlignment.Center
+        textField.autocapitalizationType = UITextAutocapitalizationType.AllCharacters
+        textField.backgroundColor = UIColor.clearColor()
+
         
         
     }
     override func viewWillAppear(animated: Bool) {
-        print("viewWillAppear")
         super.viewWillAppear(animated)
         subscribeToKeyboardNotifications()
-        self.navigationController?.toolbarHidden = false
-        self.dismissViewControllerAnimated(true, completion: nil)
-        
-        
+        navigationController?.toolbarHidden = false
+        tabBarController?.tabBar.hidden = true
+        //Shift the toolbar  by the amount of the
+        // tabBarController so that it lies at the bottom of the
+        //view.
+        navigationController?.toolbar.layer.position.y = (navigationController?.toolbar.layer.position.y)! + (tabBarController?.tabBar.bounds.height)!
+
+
+        dismissViewControllerAnimated(true, completion: nil)
+        if didEdit {
+            didEdit = false
+            navigationController?.popToRootViewControllerAnimated(true)
+        }
     }
     
+    
+    /*
+     This function allows us to get notifications on the status of the keyboard.
+     */
     func subscribeToKeyboardNotifications(){
-        print("subscribeToKeyboardNotifications")
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:"    , name: UIKeyboardWillShowNotification, object: nil)
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide:" , name: UIKeyboardWillHideNotification, object:nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(MemeEditorView.keyboardWillShow(_:))    , name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(MemeEditorView.keyboardWillHide(_:)) , name: UIKeyboardWillHideNotification, object:nil)
     }
     
     override func viewWillDisappear(animated: Bool) {
-        print("viewWillDisappear")
-        super.viewWillDisappear(animated)
         unsubscribeToKeyboardNotifications()
+        navigationController?.toolbarHidden = true
     }
     
     func unsubscribeToKeyboardNotifications(){
-        print("unsubscribeToKeyboardNotifications")
         NSNotificationCenter.defaultCenter().removeObserver(self, name:
             UIKeyboardWillShowNotification, object: nil)
     }
     
     func keyboardWillShow(notification: NSNotification) -> Void{
-        print("keyboardWillShow")
         if bottomTextField.isFirstResponder(){
             view.frame.origin.y -= getKeyboardHeight(notification)
-        }else if topTextField.isFirstResponder(){
-            self.view.frame.origin.y = 0
+        }
+        else if topTextField.isFirstResponder(){
+            view.frame.origin.y = 0
         }
     }
     
     func keyboardWillHide(notification:NSNotification) {
-        print("keyboardWillHide")
         if bottomTextField.isFirstResponder() {
-            self.view.frame.origin.y = 0
+            view.frame.origin.y = 0
         }
-        self.navigationController?.toolbarHidden = false
-        self.navigationController?.navigationBarHidden = false
-
-        
+        navigationController?.toolbarHidden = false
+        navigationController?.navigationBarHidden = false
     }
-    
+    /*
+     We use this function to get the keyboard height so that we can scroll up the view and still see the
+     textfield we are editing even though the keyboard is shown.
+     */
     func getKeyboardHeight(notification: NSNotification) -> CGFloat {
-        print("in getKeyboardHeight")
         let userInfo = notification.userInfo
         let keyboardSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue // of CGRect
-        print(keyboardSize.CGRectValue().height)
         return keyboardSize.CGRectValue().height
     }
     
+    /*
+     This function allows us to take a picture to be used in the meme.
+     */
     @IBAction func takeAPictureUsingCamera(sender: AnyObject) {
+        topTextField.enabled = true
+        bottomTextField.enabled = true
         let imagePickerController = UIImagePickerController()
         imagePickerController.delegate = self
         imagePickerController.sourceType = UIImagePickerControllerSourceType.Camera
         
-        self.presentViewController(imagePickerController, animated: true, completion: nil)
+        presentViewController(imagePickerController, animated: true, completion: nil)
     }
+    /*
+     This function allows the user to pick a picture from the pictures in albums.
+     */
     
     @IBAction func pickAnImage(sender: AnyObject) {
-        
+        topTextField.enabled = true
+        bottomTextField.enabled = true
         let imagePickerController = UIImagePickerController()
         imagePickerController.delegate = self
-        self.presentViewController(imagePickerController, animated: true, completion: nil)
+        presentViewController(imagePickerController, animated: true, completion: nil)
+        navigationController?.toolbar.layer.position.y = (navigationController?.toolbar.layer.position.y)! + (tabBarController?.tabBar.bounds.height)!
     }
+    
+    /*
+     This function sets the picture the user selects to be the image displayed in the memeEditorView
+     */
     func imagePickerController( _picker: UIImagePickerController,
-        didFinishPickingMediaWithInfo info: [String : AnyObject]){
-            
-            if let image = info[UIImagePickerControllerOriginalImage] as? UIImage  {
-                self.pictureDisplayed.image = image
-                
-                
-            }
-            //in storyboard, set content mode of image to aspect fit
-            shareOutlet.enabled = true
-            self.dismissViewControllerAnimated(true, completion:nil)
-            
+                                didFinishPickingMediaWithInfo info: [String : AnyObject]){
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage  {
+            pictureDisplayed.image = image
+        }
+        shareOutlet.enabled = true
+        dismissViewControllerAnimated(true, completion:nil)
+        navigationController?.toolbar.layer.position.y = (navigationController?.toolbar.layer.position.y)! + (tabBarController?.tabBar.bounds.height)!
+        
     }
     
     func imagePickerControllerDidCancel( _picker: UIImagePickerController){
-        
-        self.dismissViewControllerAnimated(true, completion:nil)
-        
+        dismissViewControllerAnimated(true, completion:nil)
+        navigationController?.toolbar.layer.position.y = (navigationController?.toolbar.layer.position.y)! + (tabBarController?.tabBar.bounds.height)!
     }
+    
     func textFieldShouldReturn(textField: UITextField) -> Bool {
         textField.resignFirstResponder()
-        
         return true
     }
     
     //This function sets meme Editor View back to the launch state
     @IBAction func cancelAction(sender: AnyObject) {
+        
         pictureDisplayed.image = nil
         topTextField.text = "TOP"
         bottomTextField.text = "BOTTOM"
         shareOutlet.enabled = false
-        
+        topTextField.enabled = false
+        bottomTextField.enabled = false
+        self.navigationController?.popToRootViewControllerAnimated(true)
     }
     
-    /* This function brings up Apple's stock ACtivity View, displaying several options for sharing the meme.
-    */
+    /*
+     This function creates an object meme and then saves in into the memes structure.
+     */
     func save() {
-        //Create the meme
         
-        // let meme = Meme( text: topTextField.text!, image:
-        //   imageView.image, memedImage: memedImage)
         let meme = Meme( topText: topTextField.text!,  bottomText: bottomTextField.text!,originalImage:
             pictureDisplayed.image!, memedImage: generateMemedImage())
+        (UIApplication.sharedApplication().delegate as! AppDelegate).memes.append(meme)
     }
-    
+    /*
+     This function creates the memedImage.
+     */
     func generateMemedImage() -> UIImage
     {
-        print("beginning of generateMemedImage")
-        self.navigationController?.toolbarHidden = true
-        self.navigationController?.navigationBarHidden = true
+        navigationController?.toolbarHidden = true
+        navigationController?.navigationBarHidden = true
         // Render view to an image
-        UIGraphicsBeginImageContext(self.view.frame.size)
-        self.view.drawViewHierarchyInRect(self.view.frame,
-            afterScreenUpdates: true)
+        UIGraphicsBeginImageContext(view.frame.size)
+        view.drawViewHierarchyInRect(view.frame,
+                                          afterScreenUpdates: true)
         let memedImage : UIImage =
-        UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
-        self.navigationController?.toolbarHidden = false
-        self.navigationController?.navigationBarHidden = false
-        print("end of generateMemedImage")
-
+        navigationController?.toolbarHidden = false
+        navigationController?.navigationBarHidden = false
+        didEdit = true
         return memedImage
     }
-
-    @IBAction func shareAction(sender: AnyObject) {
-        /*
-
-        1.generate a memed image
-        2.define an instance of the ActivityViewController
-        3.pass the ActivityViewController a memedImage as an activity item
-        4.present the ActivityViewController
-*/
-        
-       let memeImage1 = generateMemedImage()
-       // let textToShare = "I'm going to share the meme in 2.0, this is just a test!"
-        
-        
-            let objectsToShare = [memeImage1]
-            //let objectsToShare = [textToShare]
-            let activityVC = UIActivityViewController(activityItems: objectsToShare, applicationActivities: nil)
-            
-            self.presentViewController(activityVC, animated: true, completion: nil)
-            print("just presented share")
-            
-            
-            //pictureDisplayed.image = nil
-            //topTextField.text = "TOP"
-            //bottomTextField.text = "BOTTOM"
-           // self.navigationController?.toolbarHidden = false
-            
-            
-            
-        }
-        func resetMemeEditorView() {
-            pictureDisplayed.image = nil
-            topTextField.text = "TOP"
-            bottomTextField.text = "BOTTOM"
-            shareOutlet.enabled = false
-        }
-        
-        
     
+    /*
+     This function calls a function to generate the memed image and that calls a different function to save it and put
+     in an array.
+     */
+    @IBAction func shareAction(sender: AnyObject) {
+        let memeImage1 = generateMemedImage()
+        let objectsToShare = [memeImage1]
+        let activityVC = UIActivityViewController(activityItems: objectsToShare, applicationActivities: nil)
+        presentViewController(activityVC, animated: true, completion: nil)
+        didEdit = true
+        activityVC.completionWithItemsHandler = {
+            (activity, success, items, error) in
+            if success {
+               
+                // call save to save the meme in an array.
+                self.save()
+                self.navigationController?.popToRootViewControllerAnimated(true)
+
+            }
+        }
+    }
     
 }
---
+++ SentMemesViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  SentMemesViewCell.swift
+//  memekss
+//
+//  Created by Kathleen Stukenborg on 4/9/16.
+//  Copyright © 2016 Kathleen Stukenborg. All rights reserved.
+//
+
+import Foundation
+import UIKit
+/* this class describes the structure of the cell for our collection view.
+ */
+class SentMemesViewCell : UICollectionViewCell {
+    
+    @IBOutlet weak var memeImage: UIImageView!
+    
+}
